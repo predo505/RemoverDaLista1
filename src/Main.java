6ee9778f15b693222a4cb45beb5dda05c782fc18
predo505 <pedroesquecido1@gmail.com>
2025-04-07 23:30:52 -0300

    Aperfeiçoamento de código.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String nomeDigitado;
+
+        ArrayList<String> nomes = new ArrayList<>();
+
+        System.out.println("Digite o nome de pelo menos 5 alunos.");
+        System.out.println(" ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite o " + (i + 1) + "º nome: ");
+            String nome = scanner.nextLine();
+
+            while (nome.trim().isEmpty()) {
+                System.out.println("O nome não pode estar vazio. Tente novamente:");
+                nome = scanner.nextLine();
+            }
+
+            nomes.add(nome);
         }
+
+        System.out.println("Nomes de alunos na lista: " + nomes);
+        System.out.println(" ");
+        System.out.println("Escolha um nome que deseja remover da lista.");
+        nomeDigitado = scanner.nextLine();
+
+        while (!nomes.contains(nomeDigitado)) {
+            System.out.println("Por favor escolha um nome que esteja na lista.");
+            nomeDigitado = scanner.nextLine();
+        }
+
+        while (!nomeDigitado.equalsIgnoreCase("S")) {
+            if (nomes.contains(nomeDigitado)) {
+                System.out.println("Tem certeza de que quer remover " + nomeDigitado + " da lista? (S/N)");
+                String confirmacao = scanner.nextLine();
+
+                if (confirmacao.equalsIgnoreCase("N")) {
+                    System.out.println("Operação cancelada. Digite o nome do(a) aluno(a) que deseja remover:");
+                    nomeDigitado = scanner.nextLine();
+                    while (!nomes.contains(nomeDigitado)) {
+                        System.out.println("Por favor escolha um nome que esteja na lista.");
+                        nomeDigitado = scanner.nextLine();
+                    }
+                }
+                else if (confirmacao.equalsIgnoreCase("S")) {
+                    nomes.remove(nomeDigitado);
+                    System.out.println("O nome do(a) aluno(a) " + nomeDigitado + " foi removido com sucesso.");
+                    break;
+                }
+                else {
+                    System.out.println("Por favor, escolha entre S (para sim) ou N (para não).");
+                    scanner.nextLine();
+                }
+            }
+        }
+
+        System.out.println("Lista final: " + nomes);
+
+        scanner.close();
     }
 }
